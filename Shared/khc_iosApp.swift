05f0942c271802832b126bffed5873176ac6d9a0
haechan77 <haechan77@gmail.com>
2022-05-26 17:13:22 +0900

    first commit

---
+++ Shared/khc_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  khc_iosApp.swift
+//  Shared
+//
+//  Created by coen on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct khc_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
